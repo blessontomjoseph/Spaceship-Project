624714ca52fcb141485c15c45bba896db2d5bb1f
Tom <blessotomjoseph@gmail.com>
2022-06-09 00:45:23 +0000

    new_purpose

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('something')
-print('more')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train_path='/kaggle/input/spaceship-titanic/train.csv'
+test_path='/kaggle/input/spaceship-titanic/test.csv'
+
+train=pd.read_csv(train_path)
+test=pd.read_csv(test_path)
+
+print(train.head(5))
