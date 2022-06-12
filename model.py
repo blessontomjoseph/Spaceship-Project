5c7b6a661faa2e54ddab3c33dd4458ea45548bcb
blessontomjoseph <blessontomjoseph@gmail.com>
2022-06-13 03:27:15 +0530

    update

--- model.py
+++ model.py
@@ -8,10 +8,14 @@ from sklearn.pipeline import Pipeline
 
 model_rf=Pipeline([('model',RandomForestClassifier())])
 
-def model_rf(trainx,trainy,valx,valy,model):
+def results(trainx,trainy,valx,valy,model):
     """function that trains a model and validates it"""
     folds=KFold(n_splits=5) #once set this seems reproduceable than settting cv in cross val score
     tr_score=cross_val_score(model,trainx,trainy,cv=folds,scoring='accuracy').mean()
     model.fit(trainx,trainy)
     vl_score=accuracy_score(model.predict(valx),valy)
     return tr_score,vl_score
+
+
+
+
--- optimization.py
+++ optimization.py
@@ -6,38 +6,46 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 # bayesian search logistic regression
-seed=5
-model=model.model_rf
-
-def objective(parameters,trainx,trainy,valx,valy,model):
-    """objective function for """
-    ne=int(parameters['n_estimators'])
-    md=int(parameters['max_depth'])
-    mss=int(parameters['min_samples_split'])
-    msl=int(parameters['min_samples_leaf'])
-    # extra features
-#     mln=parameters['max_leaf_nodes']
-#     mf=parameters['max_features']
-    model.fit(trainx,trainy)
-    preds=model.predict(valx)
-    acc=accuracy_score(preds,valy)
-    return (1-acc)
+class Bayes:
+    def __init__(self,trainx,trainy,valx,valy,model):
+        self.trainx=trainx
+        self.trainy=trainy
+        self.valx=valx
+        self.valy=valy
+        self.model=model
+        self.parameters={'n_estimators':hp.uniform('n_estimators',1,200),
+                        'max_depth':hp.uniform('max_depth',1,200),
+                        'min_samples_split':hp.uniform('min_samples_split',2,10),
+                        'min_samples_leaf':hp.uniform('min_samples_leaf',1,10)}
+#                       'max_leaf_nodes':hp.uniform('max_leaf_nodes',),
+#                       'max_features':hp.uniform('ma_features',)}
 
 
-def optimizer(trial):
-    parameters={'n_estimators':hp.uniform('n_estimators',1,200),
-               'max_depth':hp.uniform('max_depth',1,200),
-               'min_samples_split':hp.uniform('min_samples_split',2,10),
-               'min_samples_leaf':hp.uniform('min_samples_leaf',1,10)}
-#               'max_leaf_nodes':hp.uniform('max_leaf_nodes',),
-#               'max_features':hp.uniform('ma_features',)}
-    best=fmin(fn=objective,space=parameters,algo=tpe.suggest,trials=trial,max_evals=500,rstate=np.random.default_rng(seed))
-    return best
+    def objective(self):
+        """objective function for """
+        ne=int(self.parameters['n_estimators'])
+        md=int(self.parameters['max_depth'])
+        mss=int(self.parameters['min_samples_split'])
+        msl=int(self.parameters['min_samples_leaf'])
+        # extra features
+    #     mln=parameters['max_leaf_nodes']
+    #     mf=parameters['max_features']
+        # model=model_rf
+        self.model.fit(self.trainx,self.trainy)
+        preds=self.model.predict(self.valx)
+        acc=accuracy_score(preds,self.valy)
+        return (1-acc)
 
 
-def bayesian_search():
+    def optimizer(self,trial):
+        best=fmin(fn=self.objective,space=self.parameters,algo=tpe.suggest,trials=trial,max_evals=500,rstate=np.random.default_rng(5))
+        return best
+
+
+def bayesian_search(trainx,trainy,valx,valy,model):
     trial=Trials()
-    best=optimizer(trial)
+    bayes=Bayes(trainx,trainy,valx,valy,model)
+    best=bayes.optimizer(trial)
     return best
 
 
@@ -47,6 +55,10 @@ def combining_models(preds1,preds2,preds3):
     preds[preds>1]=1
     preds[preds==1]==0
     return preds
+
+
+
+
 
 
 
